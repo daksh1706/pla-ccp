f9d55f27985026402f091e55330fc09ac7eaa407
dakshmaru <dakshmaru10@gmail.com>
2026-07-28 20:52:59 +0530

    Update 112A.java

--- 112A.java
+++ 112A.java
@@ -1,18 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Fast I/O equivalent to C++ fast I/O
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String s1 = st.nextToken();
-        String s2 = st.nextToken();
-        s1 > s2
-        // zero if s1 == s2
+        // Read string 1 from line 1, string 2 from line 2
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
         int result = s1.compareToIgnoreCase(s2);
 
         if (result < 0) {
