adb92cbb9cac6baf02e42568c1735b8260f2c679
dakshmaru <dakshmaru10@gmail.com>
2026-09-03 16:55:26 +0530

    Add basic Java program to print a constant value
    Implement a simple Java program that reads a long input and prints 25.

---
+++ 630A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // We read n, but it is not needed for the calculation
+        if (scanner.hasNextLong()) {
+            long n = scanner.nextLong();
+        }
+        System.out.println(25);
+    }
+}
+
